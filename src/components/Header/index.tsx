1459f595ee2af65121c2224da6fbe36db2d61b95
Victor Muniz <victorrmunizz@gmail.com>
2024-01-02 11:55:37 -0300

    initializing project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">timer</a>
+        <a href="">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
